5746b911caac5dbaa4a19fe3c2dd7d3260b3918a
adelinodada30-lab <adelinodada30@gmail.com>
2025-08-18 12:33:27 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
-const CONFIG = {
-  API_KEY: "975e39ec8e83cf7e35230a93b8d7efaf",
-  API_URL: "https://api.the-odds-api.com/v4/sports",
-  USERNAME: "scanner2025",
-  PASSWORD: "@morInfinito30"
-};
+const API_KEY = "975e39ec8e83cf7e35230a93b8d7efaf";
+const API_URL = "https://api.the-odds-api.com/v4/sports";
